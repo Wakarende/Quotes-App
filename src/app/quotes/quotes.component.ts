507c299de23632718404cbe27bfe15de01a74ee6
Joy Kirii <joykirii@gmail.com>
2021-03-26 21:10:53 +0300

    add delete button

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter} from '@angular/core';
 import { Quotes} from '../quotes'
 
 @Component({
@@ -8,16 +8,28 @@ import { Quotes} from '../quotes'
 })
 export class QuotesComponent implements OnInit {
  quotes: Quotes[] = [
-  //  new Quotes('Joy', 'If it is to be, it is up to me', 'Joy'),
-  //  new Quotes('Joy','If sticks and stones can break my bones, then words can never hurt me', 'Burens')
+   new Quotes('Joy', 'If it is to be, it is up to me', 'Joy'),
+   new Quotes('Joy','If sticks and stones can break my bones, then words can never hurt me', 'Burens')
  ]
  
-
  addNewQuote(quotes:any) {
    quotes.length = this.quotes.length;
    quotes.id = quotes.length + 1;
    this.quotes.push(quotes)
  }
+
+ @Output() toDelete = new EventEmitter<boolean>();
+
+ deleteQuote(del:boolean) {
+   this.toDelete.emit(del);
+ }
+
+ quoteDelete(toDelete: any, index: number) {
+   if(toDelete) {
+     this.quotes.splice(index, 1);
+   }
+ }
+
   constructor() { }
 
   ngOnInit(): void {
